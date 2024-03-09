52a8556ace3d2add5b723cc14a58a01e0a7e1786
sukantasaho <ssukanta520@gmail.com>
2024-03-09 19:16:19 +0530

    commited

--- College.java
+++
@@ -1,7 +0,0 @@
-public class College
-{
-     public static void main(String[] args)
-     {
-         System.out.println("Hello");
-     }
-}
--- User.java
+++
@@ -1,7 +0,0 @@
-public class User
-{
-     public static void main(String[] args)
-     {
-         System.out.println("Hello I have modified code1 first time");
-     }
-}
